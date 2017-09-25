ec3e8462260f0ead2192269f93d056add634ce0a
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-25 14:02:22 -0700

    Done.

---
+++ src/components/MoviesList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MoviesList = ({movies}) => {
+  const renderMovies = movies.map(movie =>
+    <li key={movie.id}><Link to={`/movies/${movie.id}`}>{movie.title}</Link></li>)
+
+  return (
+    <div>
+      {renderMovies}
+    </div>
+  )
+}
+
+export default MoviesList
